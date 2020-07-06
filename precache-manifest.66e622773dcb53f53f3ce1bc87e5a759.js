1a285710b35397fd268fb4230cf051e4d1a8a75d
Rhema Makamba <rhemaexel7@gmail.com>
2020-07-06 15:49:10 +0200

    Updates

---
+++ precache-manifest.66e622773dcb53f53f3ce1bc87e5a759.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb339c94bb559cc849e14aded61404c8",
+    "url": "/IshmaeelElijah/index.html"
+  },
+  {
+    "revision": "d79a1397e949e41af79f",
+    "url": "/IshmaeelElijah/static/css/main.e7485ccd.chunk.css"
+  },
+  {
+    "revision": "c1d76c634c90905fb993",
+    "url": "/IshmaeelElijah/static/js/2.c8ad2ae4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IshmaeelElijah/static/js/2.c8ad2ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79a1397e949e41af79f",
+    "url": "/IshmaeelElijah/static/js/main.31fbaa0c.chunk.js"
+  },
+  {
+    "revision": "83fb1c1c553e0cd3966b",
+    "url": "/IshmaeelElijah/static/js/runtime-main.e11b0961.js"
+  },
+  {
+    "revision": "b8fc0bc0d4e72560680e84ca5fef2c5d",
+    "url": "/IshmaeelElijah/static/media/elie.b8fc0bc0.jpg"
+  },
+  {
+    "revision": "3267ea790888e115bf9ca1698685d8ac",
+    "url": "/IshmaeelElijah/static/media/githubfinder.3267ea79.jpg"
+  },
+  {
+    "revision": "c33534d6ff5f3edd08cdba6ba41165dc",
+    "url": "/IshmaeelElijah/static/media/itlogger.c33534d6.jfif"
+  },
+  {
+    "revision": "cab8395e2052a0e0d6dc869c0c284592",
+    "url": "/IshmaeelElijah/static/media/school.cab8395e.jpeg"
+  },
+  {
+    "revision": "4cf4c0744268cfb2d3fa6f67b5f6e0e4",
+    "url": "/IshmaeelElijah/static/media/wildberrry.4cf4c074.jpg"
+  }
+]);
